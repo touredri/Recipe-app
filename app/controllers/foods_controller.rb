2ad20b05fea81482033dc7a13ca422e861c7b1ee
touredri <dcompte223@gmail.com>
2023-08-01 13:15:35 +0000

    added food list

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -25,24 +25,11 @@ class FoodsController < ApplicationController
 
     respond_to do |format|
       if @food.save
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully created.' }
-        format.json { render :show, status: :created, location: @food }
+        format.html { redirect_to foods_url, notice: 'Food was successfully created.' }
+        # format.json { render :show, status: :created, location: @food }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /foods/1 or /foods/1.json
-  def update
-    respond_to do |format|
-      if @food.update(food_params)
-        format.html { redirect_to food_url(@food), notice: 'Food was successfully updated.' }
-        format.json { render :show, status: :ok, location: @food }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
+        # format.json { render json: @food.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -53,7 +40,7 @@ class FoodsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to foods_url, notice: 'Food was successfully destroyed.' }
-      format.json { head :no_content }
+      # format.json { head :no_content }
     end
   end
 
@@ -66,6 +53,6 @@ class FoodsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def food_params
-    params.require(:food).permit(:name)
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
   end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Food < ApplicationRecord
+  belongs_to :user
+  validates :name, presence: true
+  validates :measurement_unit, presence: true
+  validates :price, presence: true
+  validates :quantity, presence: true
+  validates :user_id, presence: true
 end
