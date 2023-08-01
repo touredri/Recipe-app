c660989e0fe9340e8ee28f47edaa1b01813fc04c
touredri <dcompte223@gmail.com>
2023-08-01 02:40:37 +0100

    linters

--- spec/views/recipe_foods/edit.html.erb_spec.rb
+++ spec/views/recipe_foods/edit.html.erb_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe 'recipe_foods/edit', type: :view do
 
   it 'renders the edit recipe_food form' do
     render
-
     assert_select 'form[action=?][method=?]', recipe_food_path(recipe_food), 'post' do
     end
   end
