5e0c07cd72f8685f046f88b13fd4ab8321c32118
Lawrence Amoafo <rekenburg777@gmail.com>
2023-08-03 12:23:04 +0000

    Generate devise and add views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def current_user
-    @current_user ||= User.first
-  end
+  before_action :authenticate_user!
 end
