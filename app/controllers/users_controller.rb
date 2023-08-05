a304214fefc4b0c9c820a41f95d1d0eb9432390e
Lawrence Amoafo <rekenburg777@gmail.com>
2023-08-05 20:14:42 +0000

    Make changes to Shopping list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class UsersController < ApplicationController
   def shopping_list
     @recipes = current_user.recipes.includes(recipe_foods: :food)
@@ -11,12 +9,12 @@ class UsersController < ApplicationController
     @all_recipe_foods.each do |recipe|
       if recipe.quantity > recipe.food.quantity
         food_name = recipe.food.name
-        food_quantities[food_name] += recipe.quantity
+        food_quantities[food_name] += recipe.quantity - recipe.food.quantity
       end
     end
 
     food_quantities.each do |food_name, total_quantity|
-      @recipe_foods << { food_name:, total_quantity: }
+      @recipe_foods << { food_name: food_name, total_quantity: total_quantity }
     end
   end
 end
